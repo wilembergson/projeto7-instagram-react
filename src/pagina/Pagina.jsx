2ff545fc336bf66fee7b71bb031414e437f8014a
wilembergson <wilembergson@yahoo.com>
2022-03-14 19:12:33 -0300

    Finish

--- src/pagina/Pagina.jsx
+++ src/pagina/Pagina.jsx
@@ -4,49 +4,75 @@ import Story from "../story/Story"
 import "./Pagina.css"
 
 export default function Pagina(){
+    const story = [
+        {imagem:"arquivos/icone-1.jpg", nome:"9gag"},
+        {imagem:"arquivos/icone-4.jpeg", nome:"meowed"},
+        {imagem:"arquivos/icone-3.png", nome:"barked"},
+        {imagem:"arquivos/icone-2.jpeg", nome:"nathanwpyle..."},
+        {imagem:"arquivos/icone-5.png", nome:"wawawiwac..."},
+        {imagem:"arquivos/icone-6.jpeg", nome:"respondeai"},
+        {imagem:"arquivos/icone-7.png", nome:"filomoderna"},
+        {imagem:"arquivos/icone-8.jpeg", nome:"memeriago."},
+    ]
+
+    const postagens = [
+        {
+            foto:"arquivos/icone-4.jpeg" ,
+            nome:"meowed",
+            fotoPostagem:"arquivos/postagem-1.jpg",
+            quemCurtiu:"arquivos/icone-6.jpeg",
+            quemCurtiuNome:"respondeai",
+            numCurtidas:"101.523" 
+        },
+        {
+            foto:"arquivos/icone-3.png" ,
+            nome:"barked",
+            fotoPostagem:"arquivos/postagem-3.png",
+            quemCurtiu:"arquivos/icone-1.jpg",
+            quemCurtiuNome:"adorable_animals",
+            numCurtidas:"99.159"
+        },
+        {
+            foto:"arquivos/icone-4.jpeg",
+            nome:"meowed",
+            fotoPostagem:"arquivos/postagem-2.png",
+            quemCurtiu:"arquivos/icone-6.jpeg",
+            quemCurtiuNome:"respondeai",
+            numCurtidas:"81.112"
+        }
+    ]
+
+    const linhaSecundario = [
+        
+        {imagem:"arquivos/secundario-2.jpeg", nome:"bad.vibes.memes"},
+        {imagem:"arquivos/secundario-3.jpg", nome:"chibdart"},
+        {imagem:"arquivos/secundario-4.jpg", nome:"razoesparaacreditar"},
+        {imagem:"arquivos/secundario-5.jpg", nome:"adorable_animals"},
+        {imagem:"arquivos/secundario-6.jpeg", nome:"smallcutecats"}
+    ]
+    
     return(
     <>
     <div class="pagina">
         <div class="conteudo">
             <div class="lado-principal">
                 <div class="stories">
-                    <Story imagem="arquivos/icone-1.jpg" nome="9gag"/>
-                    <Story imagem="arquivos/icone-4.jpeg" nome="meowed"/>
-                    <Story imagem="arquivos/icone-3.png" nome="barked"/>
-                    <Story imagem="arquivos/icone-2.jpeg" nome="nathanwpyle..."/>
-                    <Story imagem="arquivos/icone-5.png" nome="wawawiwac..."/>
-                    <Story imagem="arquivos/icone-6.jpeg" nome="respondeai"/>
-                    <Story imagem="arquivos/icone-7.png" nome="filomoderna"/>
-                    <Story imagem="arquivos/icone-8.jpeg" nome="memeriago."/>
+                    {story.map(s =>{
+                        return(<Story imagem={s.imagem} nome={s.nome}/>)
+                    })}
                     <ion-icon class="seta-stories" name="chevron-forward-outline"></ion-icon>
                 </div>
 
-                <Postagem 
-                    foto="arquivos/icone-4.jpeg" 
-                    nome="meowed"
-                    fotoPostagem="arquivos/postagem-1.jpg"
-                    quemCurtiu="arquivos/icone-6.jpeg"
-                    quemCurtiuNome="respondeai"
-                    numCurtidas="101.523"
-                />
-
-                <Postagem 
-                    foto="arquivos/icone-3.png" 
-                    nome="barked"
-                    fotoPostagem="arquivos/postagem-3.png"
-                    quemCurtiu="arquivos/icone-1.jpg"
-                    quemCurtiuNome="adorable_animals"
-                    numCurtidas="99.159"
-                />
-
-                <Postagem 
-                    foto="arquivos/icone-4.jpeg" 
-                    nome="meowed"
-                    fotoPostagem="arquivos/postagem-2.png"
-                    quemCurtiu="arquivos/icone-6.jpeg"
-                    quemCurtiuNome="respondeai"
-                    numCurtidas="81.112"
-                />
+                {postagens.map(p =>{
+                        return(<Postagem 
+                            foto={p.foto}
+                            nome={p.nome}
+                            fotoPostagem={p.fotoPostagem}
+                            quemCurtiu={p.quemCurtiu}
+                            quemCurtiuNome={p.quemCurtiuNome}
+                            numCurtidas={p.numCurtidas}
+                        />)
+                    })}
             </div>
 
             <div class="lado-secundario">
@@ -63,11 +89,9 @@ export default function Pagina(){
                         <div class="ver-tudo">Ver tudo</div>
                     </div>
 
-                    <LinhaSecundario2 imagem="arquivos/secundario-2.jpeg" nome="bad.vibes.memes"/>
-                    <LinhaSecundario2 imagem="arquivos/secundario-3.jpg" nome="chibdart"/>
-                    <LinhaSecundario2 imagem="arquivos/secundario-4.jpg" nome="razoesparaacreditar"/>
-                    <LinhaSecundario2 imagem="arquivos/secundario-5.jpg" nome="adorable_animals"/>
-                    <LinhaSecundario2 imagem="arquivos/secundario-6.jpeg" nome="smallcutecats"/>
+                    {linhaSecundario.map(ls =>{
+                        return(<LinhaSecundario2 imagem={ls.imagem} nome={ls.nome}/>)
+                    })}
                     
                     <div class="topicos-secundario">Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma</div>
                     <div class="topicos-2-secundario">© 2021 INSTAGRAM DO FACEBOOK</div>
